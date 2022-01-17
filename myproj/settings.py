059813d2dc7de11e80e7862d7857f3b9a0773981
Yerin Park <1472moment@gmail.com>
2022-01-17 14:46:26 +0900

    accounts 앱 생성 및 api token 등록

--- myproj/settings.py
+++ myproj/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "blog",
     "news",
     "discussion",
+    "accounts",
 ]
 
 MIDDLEWARE = [
--- myproj/urls.py
+++ myproj/urls.py
@@ -2,10 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -13,12 +10,7 @@ urlpatterns = [
     path("blog/", include("blog.urls")),
     path("news/", include("news.urls")),
     path("discussion/", include("discussion.urls")),
+    path("accounts/", include("accounts.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# TODO:
-urlpatterns += [
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-]

605d2bd0058503d1028bff2c05be2069551ddb77
Yerin Park <1472moment@gmail.com>
2022-01-17 15:07:43 +0900

    jwt payload에 username, first_name, last_name 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from accounts.views import TokenObtainPairView, TokenRefreshView
 
 app_name = "accounts"
+
 urlpatterns = []
 
 urlpatterns += [

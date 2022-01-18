18864c9ca5c11da36c3153579cc830087ed31e30
Yerin Park <1472moment@gmail.com>
2022-01-18 17:14:19 +0900

    회원가입 API 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from accounts.views import TokenObtainPairView, TokenRefreshView
+from accounts.views import SignupAPIView, TokenObtainPairView, TokenRefreshView
 
 app_name = "accounts"
 
@@ -8,4 +8,5 @@ urlpatterns = []
 urlpatterns += [
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/signup/", SignupAPIView.as_view(), name="sign_up"),
 ]
